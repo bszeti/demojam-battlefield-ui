e90f572afab4aa8574c377378477d81b17e6426d
bszeti <bszeti@redhat.com>
2019-11-01 14:29:57 -0400

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
-oc delete project battlefield-ui
-oc new-project --skip-config-write=true battlefield-ui
-oc adm policy add-cluster-role-to-user cluster-admin -z default -n battlefield-ui
+#oc delete project battlefield-ui
+#oc new-project --skip-config-write=true battlefield-ui
+#oc adm policy add-cluster-role-to-user cluster-admin -z default -n battlefield-ui
 oc create -f deployment.yaml -n battlefield-ui
 oc create -f service.yaml -n battlefield-ui
 oc expose service battlefield-ui -n battlefield-ui
